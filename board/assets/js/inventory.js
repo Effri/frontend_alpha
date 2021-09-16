9ae8bfb93c70e3865028082b8daee7f3830dea78
Lina <angelo.9720@gmail.com>
2021-09-17 00:12:52 +0300

    Update board

---
+++ board/assets/js/inventory.js
@@ -0,0 +1,661 @@
+var itemsData = {
+
+    //     "-1": "Маска",
+    // "-3": "Перчатки",
+    // "-4": "Штаны",
+    // "-5": "Рюкзак",
+    // "-6": "Обувь",
+    // "-7": "Аксессуар",
+    // "-8": "Нижняя одежда",
+    // "-9": "Бронежилет",
+    // "-11": "Верхняя одежда",
+    // "-12": "Головной убор",
+    // "-13": "Очки",
+    // "-14": "Аксессуар",
+
+    "250": "Маска",
+    "252": "Перчатки",
+    "253": "Низ",
+    "254": "Рюкзак",
+    "255": "Обувь",
+    "256": "Подвеска",
+    "257": "Верх",
+    "258": "Бронежилет",
+    "259": "Верхняя одежда",
+    "261": "Головной убор",
+    "262": "Очки",
+    "263": "Аксессуар",
+    // "262": "Серьги",
+    
+
+    0: "Test Item",
+    1: "Аптечка",
+    2: "Канистра",
+    3: "Чипсы",
+    4: "Пиво",
+    5: "Пицца",
+    6: "Бургер",
+    7: "Хот-Дог",
+    8: "Сэндвич",
+    9: "eCola",
+    10: "Sprunk",
+    11: "Отмычка для замков",
+    12: "Сумка с деньгами",
+    13: "Материалы",
+    14: "Наркотики",
+    15: "Сумка с дрелью",
+    16: "Военная отмычка",
+    17: "Мешок",
+    18: "Стяжки",
+    19: "Ключи от машины",
+    40: "Подарок",
+    41: "Связка ключей",
+    42: "Рем. Комплект",
+    43: "Балончик с краской",
+
+
+    20: `"На корке лимона"`,
+    21: `"На бруснике"`,
+    22: `"Русский стандарт"`,
+    23: `"Asahi"`,
+    24: `"Midori"`,
+    25: `"Yamazaki"`,
+    26: `"Martini Asti"`,
+    27: `"Sambuca"`,
+    28: `"Campari"`,
+    29: `"Дживан"`,
+    30: `"Арарат"`,
+    31: `"Noyan Tapan"`,
+
+    100: "Pistol",
+    101: "Combat Pistol",
+    102: "Pistol .50",
+    103: "SNS Pistol",
+    104: "Heavy Pistol",
+    105: "Vintage Pistol",
+    106: "Marksman Pistol",
+    107: "Revolver",
+    108: "AP Pistol",
+    109: "Stun Gun",
+    110: "Flare Gun",
+    111: "Double Action",
+    112: "Pistol Mk2",
+    113: "SNSPistol Mk2",
+    114: "Revolver Mk2",
+
+    115: "Micro SMG",
+    116: "Machine Pistol",
+    117: "SMG",
+    118: "Assault SMG",
+    119: "Combat PDW",
+    120: "MG",
+    121: "Combat MG",
+    122: "Gusenberg",
+    123: "Mini SMG",
+    124: "SMG Mk2",
+    125: "Combat MG Mk2",
+
+    126: "Assault Rifle",
+    127: "Carbine Rifle",
+    128: "Advanced Rifle",
+    129: "Special Carbine",
+    130: "Bullpup Rifle",
+    131: "Compact Rifle",
+    132: "Assault Rifle Mk2",
+    133: "Carbine Rifle Mk2",
+    134: "Special Carbine Mk2",
+    135: "Bullpup Rifle Mk2",
+
+    136: "Sniper Rifle",
+    137: "Heavy Sniper",
+    138: "Marksman Rifle",
+    139: "Heavy Sniper Mk2",
+    140: "Marksman Rifle Mk2",
+
+    141: "Pump Shotgun",
+    142: "SawnOff Shotgun",
+    143: "Bullpup Shotgun",
+    144: "Assault Shotgun",
+    145: "Musket",
+    146: "Heavy Shotgun",
+    147: "Double Barrel Shotgun",
+    148: "Sweeper Shotgun",
+    149: "Pump Shotgun Mk2",
+
+    180: "Нож",
+    181: "Дубинка",
+    182: "Молоток",
+    183: "Бита",
+    184: "Лом",
+    185: "Гольф клюшка",
+    186: "Бутылка",
+    187: "Кинжал",
+    188: "Топор",
+    189: "Кастет",
+    190: "Мачете",
+    191: "Фонарик",
+    192: "Швейцарский нож",
+    193: "Кий",
+    194: "Ключ",
+    195: "Боевой топор",
+
+    200: "Пистолетный калибр",
+    201: "Малый калибр",
+    202: "Автоматный калибр",
+    203: "Снайперский калибр",
+    204: "Дробь",
+
+    // Fishing
+    205: "Удочка",
+    206: "Улучшенная удочка",
+    207: "Удочка MK2",
+    208: "Наживка",
+    209: "Корюшка",
+    210: "Кунджа",
+    211: "Лосось",
+    212: "Окунь",
+    213: "Осётр",
+    214: "Скат",
+    215: "Тунец",
+    216: "Угорь",
+    217: "Чёрный амур",
+    218: "Щука",
+
+    //Farmer Job Items
+    219: "Урожай",
+    220: "Семена",
+    199: "Подарок",
+
+    221: "Водка с лимоном",
+    222: "Водка на бруснике",
+    223: "Русский стандарт",
+    224: "Коньяк Дживан",
+    225: "Коньяк Арарат",
+    226: "Пиво разливное",
+    227: "Пиво бутылочное",
+    228: "Кальян",
+    229: "Фишки",
+
+
+
+    777: "Рем.Комплект",
+};
+
+var itemsInfo = {
+    // "-1": "Маска - поможет скрыть твою личность.",
+    // "-3": "Перчатки.",
+    // "-4": "Штаны.",
+    // "-5": "Рюкзак.",
+    // "-6": "Обувь.",
+    // "-7": "Украшения/Галстук.",
+    // "-8": "Нижния одежда.",
+    // "-9": "Бронежилет - поможет получать меньже урона от выстрела.",
+    // "-10": ".",
+    // "-11": "Верхняя одежда.",
+    // "-12": "Головные уборы.",
+    // "-13": "Очки.",
+    // "-14": "Часы/Брелки на руку.",
+    1: "Аптечка - поможет реанимировать или восстановить себе здоровье.",
+    2: "Конистра бензина - поможет заправить ТС в любом месте.",
+    3: "Пачка чипсов - поможет восстановить голод.",
+    4: "Без алкольное пиво - дописать.",
+    5: "Пицца - поможет восстановить голод.",
+    6: "Бургер - поможет восстановить голод.",
+    7: "Хот-Дог - поможет восстановить голод.",
+    8: "Сэндвич - поможет восстановить голод.",
+    9: "Cola - поможет восстановить жажду.",
+    10: "Sprunk - поможет восстановить жажду.",
+    11: "Отмычка - поможет вскрыть замок.",
+    12: "Сумка с деньгами - скорее всего деньги ворованые.",
+    13: "Материалы - помогут скрафтить оружие.",
+    14: "Пакетик марихуаны - поможет восстановить здоровье.",
+    15: "Сумка с дрелью - поможет вскрыть хранилище.",
+    16: "Военная отмычка - поможет скрыть военую технику.",
+    17: "Мешок - надев его на человека он не будет видеть.",
+    18: "Стяжки - помогут связать человека.",
+    19: "Ключи - помогут открыть ваше личное ТС.",
+    20: "Даёт алкогольный эффект.",
+    21: "Даёт алкогольный эффект.",
+    22: "Даёт алкогольный эффект.",
+    23: "Даёт алкогольный эффект.",
+    24: "Даёт алкогольный эффект.",
+    25: "Даёт алкогольный эффект.",
+    26: "Даёт алкогольный эффект.",
+    27: "Даёт алкогольный эффект.",
+    28: "Даёт алкогольный эффект.",
+    1: "Аптечка - поможет реанимировать или восстановить себе здоровье.",
+    42: "Рем. Коплект - починит ваш автомобиль.",
+    43: "Балончик - можно нарисовать граффити.",
+};
+
+var infos = {
+    1: "Информация какая-то типа",
+    114: "Ревальвер это",
+    254: "чет там",
+
+};
+
+Vue.component('item', {
+    template: '<div class="item draggable" :class="items" :id="ids" :fastslot="fast_slot" :data-type="typ" :data-active="ativ" @click.right.prevent="select">\
+    <img :src="src"><span>{{count}} Шт</span></div>',
+    props: ['id', 'index', 'count', 'isactive', 'type', 'subdata', 'fast_slot'],
+    data: function () {
+        return {
+            src: './assets/images/icons/items/' + this.id + '.png',
+            name: itemsData[this.id],
+            ids: this.index,
+            typ: this.type,
+            ativ: (this.isactive == 1) ? `active` : 0,
+            items: `i${this.id}a${this.isactive}`,
+        }
+    },
+    methods: {
+        select: function (event) {
+            board.selectFastSlot = !1;
+            if (board.sType == (this.type == 'inv') ? 1 : 0) {
+                if (board.sIndex == this.index) {
+                    context.hide()
+                } else {
+                    board.sType = (this.type == 'inv') ? 1 : 0;
+                    board.sID = this.id;
+                    board.sIndex = this.index;
+                    context.type = (this.type == 'inv') ? 1 : 0;
+                    context.fastSlot = this.fast_slot;
+                    let text = ""
+                    text += infos[this.id] == undefined ? "Нет информации" : infos[this.id];
+                    if (this.subdata)
+                        text += " [" + this.subdata + "]"
+
+                    board.context(event, itemsData[this.id], text)
+                }
+            } else {
+                board.sType = (this.type == 'inv') ? 1 : 0;
+                board.sID = this.id;
+                board.sIndex = this.index;
+                context.type = (this.type == 'inv') ? 1 : 0;
+
+                let text = ""
+                text += infos[this.id] == undefined ? "Нет информации" : infos[this.id];
+                if (this.subdata)
+                    text += " [" + this.subdata + "]"
+
+                board.context(event, itemsData[this.id], text)
+            }
+            // console.log(`====================`)
+            // console.log(`index ${this.index}`)
+            // console.log(`contype ${context.type}`)
+            // console.log(`type ${this.type}`)
+            // console.log(`====================`)
+        }
+    }
+})
+
+var board = new Vue({
+    el: ".inv",
+    data: {
+        active: true,
+        outside: false,
+        outType: 0,
+        outHead: "Багажник",
+        item: ["Уровень", "Предупреждения", "Дата создания", "Номер телефона", "Номер счёта", "Номер паспорта", "Организация", "Ранг", "Работа", "Статус"],
+        //		0        1  	 2 		     3 		        4	   5            6		  7			  8				9	  10	  11        12	      13    
+        stats: ["15", "30/60", "777 777", "Администрация", "2", "A B C D", "01.05.1980", "Строитель", "CityHall", "17", "Vovan", "Putin", "333 666", "4276 7700"],
+        items: [
+            [250, 5, 1],
+            [251, 5, 1],
+            [252, 5, 1],
+            [253, 5, 1],
+            [254, 5, 1],
+            [255, 5, 1],
+            [256, 5, 1],
+            [257, 5, 1],
+            [258, 5, 1],
+            [259, 5, 1],
+            [260, 5, 1],
+            [261, 5, 1],
+            [262, 5, 1],
+            [5, 10, 0],
+            [10, 500, 0],
+            [11, 100, 0],
+            [5, 10, 0],
+            [10, 500, 0],
+            [11, 100, 0],
+            [5, 10, 0],
+            [10, 500, 0],
+            [11, 100, 0]
+        ],
+        outitems: [
+            [1, 5],
+            [5, 10],
+            [10, 500]
+        ],
+        sIndex: 0,
+        sType: 0,
+        sID: 0,
+        key: 0,
+        money: 0,
+        donate: 0,
+        bank: 0,
+        page: 1,
+        statis: 0,
+        sIndex: 0,
+        sType: 0,
+        sID: 0,
+        key: 0,
+        arraymax: 0,
+        balance: 0,
+        menu: 0,
+        totrans: null,
+        aftertrans: null,
+        fname: null,
+        pause: 0,
+        lname: null,
+        sFastSlot: -1,
+		safeindex: [-1,-1,-1,-1,-1],
+		selectFastSlot: false,
+		savedFast: [undefined, undefined, undefined, undefined, undefined],
+		arraymax: 0
+    },
+    methods: {
+        close: function () {
+            this.active = false
+            this.balance = 0;
+            this.menu = 0;
+            this.totrans = null;
+            this.aftertrans = null;
+            this.fname = null;
+            this.lname = null;
+        },
+        onInputTrans: function () {
+            if (!this.check(this.totrans)) {
+                this.totrans = null;
+                this.aftertrans = null;
+            } else {
+                if (Number(this.totrans) < 0) this.totrans = 0;
+                this.aftertrans = Number(this.totrans) * 1000;
+            }
+        },
+        onInputName: function () {
+            if (this.check(this.fname) || this.check(this.lname)) {
+                this.fname = null;
+                this.lname = null;
+            }
+        },
+        check: function (str) {
+            return (/[^a-zA-Z]/g.test(str));
+        },
+        back: function () {
+            this.menu = 4;
+        },
+        open: function (id) {
+            this.menu = id;
+        },
+
+        wheel: function (id) {
+            this.pause = new Date().getTime();
+            let data = null;
+            mp.trigger("wheel", id, data);
+        },
+        buy: function (id) {
+            if (new Date().getTime() - this.pause < 5000) {
+                mp.events.call('notify', 4, 9, "Подождите 5 секунд", 3000);
+                return;
+            }
+            this.pause = new Date().getTime();
+            let data = null;
+            switch (id) {
+                case 1:
+                    data = this.fname + "_" + this.lname;
+                    break;
+                case 2:
+                    data = this.totrans;
+                    break;
+                case 9:
+                    data = this.totrans;
+                    break;
+                default:
+                    break;
+            }
+            mp.trigger("donbuy", id, data);
+        },
+        show: function (stars) {
+            this.balance = stars;
+        },
+        hide: function (event) {
+            context.hide()
+        },
+        outSet: function (json) {
+            this.key++
+            this.outType = json[0]
+            this.outHead = json[1]
+            this.outitems = json[2]
+        },
+        pages: function (id) {
+            this.page = id;
+        },
+        statsid: function (id) {
+            this.statis = id;
+        },
+        context: function (event, name, sub) {
+            if (clickInsideElement(event, 'item')) {
+                context.show(event.pageX, event.pageY, name, sub)
+            } else {
+                context.hide()
+            }
+        },
+        hide: function (event) {
+            context.hide()
+        },
+        outSet: function (json) {
+            this.key++
+            this.outType = json[0]
+            this.outHead = json[1]
+            this.outitems = json[2]
+        },
+        itemsSet: function (json) {
+            this.key++
+            this.items = json
+			
+			this.keys = 0;
+			for (let i = 0; i < this.savedFast.length; i++)
+			{
+				if (this.savedFast[i] != undefined)
+					this.keys += 1;
+			}
+			
+			let keys = 0;
+			this.safeindex = [-1,-1,-1,-1,-1];
+			
+			for (let i = 0; i < this.items.length; i++)
+			{
+				var item = this.items[i]
+				for (let t = 0; t < 6; t++)
+				{
+					if (this.savedFast[t] != undefined && item[0] == this.savedFast[t][0] && item[1] == this.savedFast[t][1] )
+					{
+						this.safeindex[t] = i;
+					}
+				}
+				
+			}
+			
+			for(let i = 0; i < this.safeindex.length; i++)
+			{
+				if (this.safeindex[i] != -1)
+				{
+					mp.trigger("bind", this.safeindex[i], i);
+				}
+			}
+			
+			
+			if (keys != this.keys)
+			{
+				for (let i = 0; i < this.savedFast.length; i++)
+				{
+					var find = false;
+					for (let t = 0; t < this.items.length; t++)
+					{
+						if (this.items[t][0] == this.savedFast[i][0] && this.items[t][1] == this.savedFast[i][1])
+							find = true;
+					}
+					if (!find)
+					{
+						this.savedFast.splice(i, 1);
+						this.savedFast[i] = undefined;
+					}
+				}
+			}
+			
+        },
+        itemUpd: function (index, data) {
+            this.key++
+            this.items[index] = data
+        },
+        send: function (id) {
+            // console.log(`Действие ${id}`)
+            let type = (this.sType) ? 0 : this.outType
+            mp.trigger('boardCB', id, type, this.sIndex)
+        },
+        useFastSlot: function(t) 
+		{
+			if (this.items[this.sIndex][0] > 99 && this.items[this.sIndex][0] < 196 || this.items[this.sIndex][0] == 258){
+            this.savedFast[t] = this.items[this.sIndex] ,this.selectFastSlot = false, this.sFastSlot = t, mp.trigger("bind", this.sIndex, this.sFastSlot)
+			}
+        },
+        unsetFastSlot: function() {
+            let t = this.items[this.sIndex];
+			let id = 0;
+			for (let i = 0; i < this.savedFast.length; i++)
+			{
+				if (this.savedFast[i] != undefined && this.savedFast[i][0] == t[0] && this.savedFast[i][1] == t[1])
+					id = i;
+			}
+            this.savedFast[id] = undefined; 
+			this.key++;
+			mp.trigger("unbind", id);
+        },
+        shieldact: function (id, type, index) {
+            // console.log(`Действие ${id} Вещь ${index}`)
+            // console.log(`====================`)
+            mp.trigger('boardCB', id, type, index)
+        }
+    },
+})
+
+var context = new Vue({
+    el: ".info",
+    data: {
+        active: false,
+        style: '',
+        type: true,
+        name: "Ключ",
+        subtext: "Серийник",
+        fastSlot: -1
+    },
+    methods: {
+        show: function (x, y, name, sub) {
+            this.style = `left:${x+25}px;top:${y}px;`
+            this.name = name;
+            this.subtext = sub;
+            this.active = true
+        },
+        hide: function () {
+            this.active = false;
+            this.name = "";
+            this.subtext = "";
+            board.sType = null;
+            board.sID = null;
+            board.sIndex = null
+        },
+        setFastSlot: function() {
+            this.hide(), board.selectFastSlot = true
+        },
+        unsetFastSlot: function() {
+            this.hide(), board.unsetFastSlot()
+        },
+        btn: function (id) {
+            if (id == 5)
+			{
+				this.setFastSlot();
+				return;
+			}
+			if (id == 6)
+			{
+				this.unsetFastSlot();
+				return;
+			}
+            this.hide()
+            board.send(id)
+        }
+    }
+})
+
+function clickInsideElement(e, className) {
+    var el = e.srcElement || e.target;
+    if (el.classList.contains(className)) {
+        return el;
+    } else {
+        while (el = el.parentNode) {
+            if (el.classList && el.classList.contains(className)) {
+                return el;
+            }
+        }
+    }
+    return false;
+}
+
+DragManager.onDragCancel = function (dragObject) {
+    dragObject.avatar.rollback();
+    //console.log(`sss`)
+};
+
+DragManager.onDragEnd = function (dragObject, dropElem, action) {
+    // console.log(`Заканчиваю действие`)
+    var sindex = $(dragObject.elem).attr('id')
+    var temptype = $(dragObject.elem).data('type')
+    var type;
+    if (temptype == 'inv' || temptype == 'eq') {
+        type = 0
+    } else {
+        switch (board.outHead) {
+            case "Багажник":
+                type = 2;
+                break;
+            case "Шкаф с предметами":
+                type = 3;
+                break;
+            case "Шкаф с одеждой":
+                type = 4;
+                break;
+            case "Склад оружия":
+                type = 6;
+                break;
+            case "Связка ключей":
+                type = 7;
+                break;
+            case "Оружейный сейф":
+                type = 8;
+                break;
+            case "Обмен":
+                type = 8;
+                break;
+        }
+    }
+    //   console.log(`=========`)
+    //   console.log(`====================`)
+    //   console.log(`index ${sindex}`)
+    //   console.log(`temptype ${temptype}`)
+    //   console.log(`type ${type}`)
+    //   console.log(`Действие ${action}`)
+    //   console.log(`====================`)
+    //dragObject.avatar.rollback();  
+    board.shieldact(action, type, sindex)
+    dragObject.avatar.rollback();
+    //   console.log($(dragObject.elem).data("name"))
+    //   console.log($(dropElem).data("action"))
+    //   dragObject.elem.style.display = 'none';
+    //   console.log(action)
+    //   console.log(`Выкинул`)
+    //  var act = $(dropElem).data("action")
+    //   if(!act) return;
+};
